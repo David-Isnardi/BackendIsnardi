f7c1ba876df91cd119055c99b420489c53462aa9
davidisnardi <davidisnardi38@gmail.com>
2023-12-07 20:34:48 -0300

    DesafioManejoDeArchivos

--- ProductManager.js
+++ ProductManager.js
@@ -1,83 +1,107 @@
-const fs = require("node:fs");
+import {promises as fs, readFile} from "fs"
 
-class ProductManager {
-    constructor() {
-     this.products = []
-   };
-   
-   addProduct(title, description, price, thumbnail, code, stock){
-    if (this.ValidateField(title, description, price, thumbnail, code, stock) && this.DuplicateCode(code)){
-     const newProduct = {
-      id: this.IdGenerator(),
-      title: title,
-      description: description,
-      price: price,
-      thumbnail: thumbnail,
-      code: code,
-      stock: stock
-      };
-   
-     this.products.push(newProduct)
-     console.log(this.products)
-    };
-   };
-   
-   getProducts() {
-    return this.products
-   };
-   
-   getProductsById(Productid){
-    const result = this.products.find((product) => product.id === Productid);
-    if(!result) {
-     return console.log("Not found")
+class ProductManager{
+    constructor(){
+        this.patch = "./productos.txt"
+        this.products = []
     }
-    return result
-   };
-   
-   IdGenerator() {
-     let id = 0;
-      if (this.products.length === 0) {
-       id = 1; 
-   
-     } else {
-       id = this.products[this.products.length - 1].id + 1;
-     };
-     
-   return id;
-   };
-   
-   DuplicateCode(code){
-     if (this.products.find((product) => product.code === code) !== undefined) {
-     console.error("Este producto ya existe")
-     return false;
-   
-   } else {
-     return true;
-   };
-   };
-   
-   
-   ValidateField(title, description, price, thumbnail, code, stock) {
-   if (!title || !description || !price || !thumbnail || !code || !stock) {
-     console.error("Por favor, completa los campos");
-     return false;
-   
-   } else {
-     return true;
-   };
-   };
-   
-   
-   };
-   
-   const newProductManager = new ProductManager();
-   
-   newProductManager.addProduct("Pan", "Bimbo", 300, "SinImagen", "123", 10);
-   newProductManager.addProduct("Queso", "Calcar", 100, "SinImagen", "124", 5);
-   //Codigo Duplicado
-   newProductManager.addProduct("Queso", "Calcar", 100, "SinImagen", "124", 5);
-   //Campo sin completar
-   newProductManager.addProduct("Pan", 300, "123", 10);
-   
-   console.log(newProductManager.getProductsById(1)) 
-   
+
+    static id = 0
+    
+    addProduct = async (title, description, price, thumbnail, code, stock) => {
+
+        ProductManager.id++
+
+        let newProduct = {
+            id: ProductManager.id,
+            title,
+            description,
+            price,
+            thumbnail,
+            code,
+            stock
+        };
+
+        this.products.push(newProduct)
+
+        console.log(newProduct)
+
+       
+       await fs.writeFile(this.patch, JSON.stringify(this.products))
+    }
+
+    readProducts = async () => {
+        let respuesta = await fs.readFile(this.patch, "utf-8")
+        return(JSON.parse(respuesta))
+    }
+
+    getProducts = async () => {
+        let respuesta2 = await this.readProducts()
+        return console.log(respuesta2)
+    }
+
+    getProductsById = async (id) => {
+
+        let respuesta3 = await this.readProducts()
+        if (!respuesta3.find(product => product.id === id)){
+            console.log("Not found")
+        } else {
+        console.log(respuesta3.find(product => product.id === id))
+        }
+    }
+
+    deleteProductsById = async (id) => {
+        let respuesta3 = await this.readProducts()
+        let productfilter = respuesta3.filter(products => products.id != id)
+        await fs.writeFile(this.patch, JSON.stringify(productfilter))
+        console.log("Producto eliminado")
+    }
+
+    updateProducts = async ({id, ...producto}) => {
+
+        await this.deleteProductsById(id);
+
+        let productOld = await this.readProducts()
+
+        let productsModified = [{id, ...producto}, ...productOld]
+
+        await fs.writeFile(this.patch, JSON.stringify(productsModified))
+
+    }
+}
+
+const productos = new ProductManager
+
+
+//PRODUCTOS
+productos.addProduct("Pan" , "Bimbo", 1000 , "Sinimagen", "abc123", 10)
+productos.addProduct("Leche" , "Conaprole", 500, "Sinimagen", "abc456", 5)
+productos.addProduct("Queso" , "Dambo", 1000, "Sinimagen", "abc789", 15)
+//-------------------------------------------------------------------------
+
+//VER TODOS LOS PRODUCTOS
+//productos.getProducts()
+//-----------------------
+
+//TRAER PRODUCTO POR SU ID
+//productos.getProductsById(3)
+//------------------------
+
+//ELIMINAR PRODUCTO POR SU ID
+//productos.deleteProductsById(2)
+//---------------------------
+
+
+//ACTUALIZAR PRODUCTO
+/*productos.updateProducts({
+  id: 1,
+  title: 'Pan',
+  description: 'Bimbo',
+  price: 2,
+  thumbnail: 'Sinimagen',
+  code: 'abc123',
+  stock: 10
+}
+)*/
+//-------------------------------------
+
